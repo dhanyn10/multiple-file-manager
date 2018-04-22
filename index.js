7cd4f0346284977b9366769d860613681cd77e50
Dhany Nurdiansyah <dhanyn.dhan@gmail.com>
2018-04-22 20:47:49 +0700

    set pathname for modified index file

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function createWindow () {
 
     // and load the index.html of the app.
     win.loadURL(url.format({
-        pathname: path.join(__dirname, 'index.htm'),
+        pathname: path.join(__dirname, 'index.html'),
         protocol: 'file:',
         slashes: true
     }))
