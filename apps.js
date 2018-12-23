f91ca3ad7378f4d707c36c581273a77560cd6522
dhanyn10 <dhanyn.dhan@gmail.com>
2018-12-23 09:46:55 +0700

    update

--- apps.js
+++ apps.js
@@ -1,20 +1,20 @@
 $("#rename-options").on('change', function(){
     var value = $("#rename-options").val();
-    if(value == "delete")
+    if(value == "delete-character")
     {
         $(".rename").each(() => {
             $(".rename").removeClass("visible");
         });
         $("#delete-c").addClass("visible");
     }
-    if(value == "replace")
+    if(value == "replace-character")
     {
         $(".rename").each(() => {
             $(".rename").removeClass("visible");
         });
         $("#replace-c").addClass("visible");
     }
-    if(value == "insert")
+    if(value == "insert-character")
     {
         $(".rename").each(() => {
             $(".rename").removeClass("visible");
