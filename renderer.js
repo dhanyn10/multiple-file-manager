b3c091270f48157008e29d16b0dca321f131946d
dhanyn10 <dhanyn.dhan@gmail.com>
2018-12-25 10:24:54 +0700

    update

--- renderer.js
+++ renderer.js
@@ -195,42 +195,100 @@ $("#apply-manage").click(function(){
                 {
                     //initiate variable to store new file name
                     newfilename             = "";
-                    if(selected_function == "delete-duplicated-file")
+                    file.forEach(function (filename)
                     {
-                        //sort all file name in current directory
-                        arrfilename = arrfilename.sort();
-                        for(a = 0; a < arrfilename.length; a++)
+                        /**
+                        * =========================================================================
+                        * get file name without it's extension, assuming file has complete name
+                        * like mytext.pdf, so we will get filename : "mytext" without pdf extension
+                        * =========================================================================
+                        */
+                        temporaryfilename   = filename.split(".");
+                        filenameonly        = temporaryfilename[0];
+
+                        /**
+                         * =========================================================================
+                         * we need to insert all file name to
+                         * >>arrfilename<< then sort it when usen need to run the function
+                         * =========================================================================
+                         */
+                        arrfilename.push(filenameonly);
+                    });
+                    //sort all file name in current directory
+                    arrfilename = arrfilename.sort();
+                    for(a = 0; a < arrfilename.length; a++)
+                    {
+                        duplicatedfile = false;
+                        for(c = 0; c < arrfileunique.length; c++)
                         {
-                            duplicatedfile = false;
-                            for(c = 0; c < arrfileunique.length; c++)
-                            {
-                                /**
-                                    * ============================================================================
-                                    * Looking for any similarity in filename with javascript >>indexOf<< function.
-                                    * if this indexOf function return value with > -1, then it's mean there is any
-                                    * similarity in filename and set boolean:duplicatedfile to "true"
-                                    * ============================================================================
-                                    */
-                                if(arrfilename[a].indexOf(arrfileunique[c]) > -1)
-                                {
-                                    duplicatedfile = true;
-                                }
-                            }
                             /**
-                                * ================================================================================
-                                * at the rest of above loop to find any duplicated file. If system don't
-                                * find any dupplicated file based on filename, current filename will be stored
-                                * into >>arrfileunique<<
-                                * ================================================================================
+                                * ============================================================================
+                                * Looking for any similarity in filename with javascript >>indexOf<< function.
+                                * if this indexOf function return value with > -1, then it's mean there is any
+                                * similarity in filename and set boolean:duplicatedfile to "true"
+                                * ============================================================================
                                 */
-                            if(duplicatedfile == false)
+                            if(arrfilename[a].indexOf(arrfileunique[c]) > -1)
                             {
-                                arrfileunique.push(arrfilename[a]);
+                                duplicatedfile = true;
                             }
                         }
-                        //function delete duplicated file
-                        deleteduplicatedfile(fs, directory_location, file, arrfilename, arrfileunique);
+                        /**
+                            * ================================================================================
+                            * at the rest of above loop to find any duplicated file. If system don't
+                            * find any dupplicated file based on filename, current filename will be stored
+                            * into >>arrfileunique<<
+                            * ================================================================================
+                            */
+                        if(duplicatedfile == false)
+                        {
+                            arrfileunique.push(arrfilename[a]);
+                        }
                     }
+                    //function delete duplicated file
+                    file.forEach(function(filename){
+                        deletethisfile  = true;
+                        for(a = 0; a < arrfileunique.length; a++)
+                        {
+                            //split filename with dot (.)
+                            arrname         = filename.split(".");
+                            //get file name extension
+                            fileextension   = "." + arrname[arrname.length-1];
+                            //set value for uniquefilename
+                            uniquefilename  = arrfileunique[a] + fileextension;
+                            //if filename and uniquefilename is same
+                            if(filename == uniquefilename)
+                            {
+                                deletethisfile = false;
+                            }
+                        }
+                        if(deletethisfile == true)
+                        {
+                            fs.unlink(directory_location + filename, function (error)
+                            {
+                                if(error)
+                                {
+                                    reportresult(
+                                        "LI",   //created element
+                                        "list-group-item danger", //element class name
+                                        "Delete Duplicated File", //function name
+                                        "Error : [" + error + "]", //text node
+                                        "resultdetails" //target element
+                                    );
+                                }
+                                else
+                                {
+                                    reportresult(
+                                        "LI",   //created element
+                                        "list-group-item", //element class name
+                                        "Delete Duplicated File", //function name
+                                        " : [" + filename + "] success", //text node
+                                        "resultdetails" //target element
+                                    );
+                                }
+                            });
+                        }
+                    });
                 }
             });
         }
@@ -249,102 +307,45 @@ function rename(fn, fs, fulldir, filename, newfile)
     fs.rename(fulldir + filename, fulldir + newfile, function(error){
         if(error)
         {
-            htmlresult = "<li class='list-group-item text-danger'>" + error + "</li>";
-            $("#resultdetails").append(htmlresult);
+            
+            reportresult(
+                "LI",   //created element
+                "list-group-item danger", //element class name
+                fn, //function name
+                "Error : [" + error + "]", //text node
+                "resultdetails" //target element
+            );
         }
         else
         {
-            fn = fn.replace('-', ' ');
-            listgroup   = document.getElementById('resultdetails');
-            htmlresult  = "<li class='result-item list-group-item'>" + fn + " : [" + filename + "] <i class='fas fa-angle-double-right'></i> [" + newfile + "] success</li>";
-            $(htmlresult).insertBefore(listgroup);
+            reportresult(
+                "LI",   //created element
+                "list-group-item", //element class name
+                fn, //function name
+                " : [" + filename + "] to [" + newfile + "] success", //text node
+                "resultdetails" //target element
+            );
         }
     });
 }
-function deleteduplicatedfile(fs, fulldir, file, arrfilename, arrfileunique)
+
+function reportresult(element, classname,fn, textnode, targetelement)
 {
-    //reset value of success count
-    $("#success-count").val(0);
-    success_count       = 0;
-    array_error_report  = [];
-    file.forEach(function(filename){
-        deletethisfile  = true;
-        for(a = 0; a < arrfileunique.length; a++)
-        {
-            //split filename with dot (.)
-            arrname         = filename.split(".");
-            //get file name extension
-            fileextension   = "." + arrname[arrname.length-1];
-            //set value for uniquefilename
-            uniquefilename  = arrfileunique[a] + fileextension;
-            //if filename and uniquefilename is same
-            if(filename == uniquefilename)
-            {
-                deletethisfile = false;
-            }
-        }
-        if(deletethisfile == true)
-        {
-            fs.unlink(fulldir + filename, function (error)
-            {
-                if (error)
-                {
-                    array_error_report.push(error);
-                    $("#error-report").val(array_error_report);
-                }
-                else
-                {
-                    success_count++;
-                    $("#success-count").val(success_count);
-                }
-            });
-        }
-    });
-
-    setTimeout(function(){
-        
-        errorReport     = $("#error-report").val();
-        successCount    = $("#success-count").val();
-        reportMessage   = "";
-
-        if(errorReport > 0)
-        {
-            reportMessage += "Error Description: \n" + errorReport + "\n";
-        }
-        if(successCount > 0)
-        {
-            reportMessage += "Success executing: " + successCount + " files";
-        }
-        bootbox.dialog({
-            title: "Result",
-            message: reportMessage,
-            buttons: {
-                confirm: {
-                    label: "Okay",
-                    className: "btn-success"
-                }
-            },
-            closeButton: false,
-            callback: function (result) {
-                //none
-            }
-        });
-    }, 500);
+    fn = fn.replace("-", " ");
+    newreport = document.createElement(element);
+    newreport.className = classname;
+    txtnode = document.createTextNode(fn + textnode);
+    newreport.appendChild(txtnode);
+    resultdetails = document.getElementById(targetelement);
+    resultdetails.insertBefore(newreport, resultdetails.childNodes[0]);
 }
 
 window.onerror = function(error, url, line) {
-    bootbox.dialog({
-        title: "Error",
-        message: "Description: \n" + error + "\n Line: " + line,
-        buttons: {
-            confirm: {
-                label: "Okay",
-                className: "btn-danger"
-            }
-        },
-        closeButton: false,
-        callback: function (result) {
-            //none
-        }
-    });
+    reportresult(
+        "LI",   //created element
+        "list-group-item text-danger", //element class name
+        "Error", //function name
+        " : [" + error + "]", //text node
+        "resultdetails" //target element
+    );
 }
