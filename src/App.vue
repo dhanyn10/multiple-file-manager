1b040a2b00193f2641613f983e884c656ebd88cf
dhanyn10 <dhanyn.dhan@gmail.com>
2020-05-02 15:29:23 +0700

    add components to main app

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,30 @@
 <template>
     <div>
         <Navbar/>
+        <b-container>
+            <FormSearch/>
+            <ListFile/>
+        </b-container>
     </div>
 </template>
 
 <script>
+import Vue from 'vue'
+
 import Navbar from './components/Navbar.vue'
+import FormSearch from './components/FormSearch.vue'
+import ListFile from './components/ListFile.vue'
+
+import { BContainer } from 'bootstrap-vue'
+
+Vue.component('b-container', BContainer)
 
 export default {
     name: 'App',
     components: {
-        Navbar
+        Navbar,
+        FormSearch,
+        ListFile
     }
 }
 </script>
