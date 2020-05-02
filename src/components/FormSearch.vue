bea5d98062ac61ed7bcdaebfcf297d6a460f0265
dhanyn10 <dhanyn.dhan@gmail.com>
2020-05-02 14:31:46 +0700

    add component search

---
+++ src/components/FormSearch.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <b-form>
+            <b-form-row description="browse directory here">
+                <b-col cols="8">
+                    <b-form-input disabled></b-form-input>
+                </b-col>
+                <b-col cols="4">
+                    <b-button block variant="info">browse</b-button>
+                </b-col>
+            </b-form-row>
+        </b-form>
+    </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import { BForm, BFormGroup, BFormInput, BButton } from 'bootstrap-vue'
+
+Vue.component('b-form', BForm)
+Vue.component('b-form-group', BFormGroup)
+Vue.component('b-form-input', BFormInput)
+Vue.component('b-button', BButton)
+
+export default {
+    name: 'FormSearch'
+}
+</script>
