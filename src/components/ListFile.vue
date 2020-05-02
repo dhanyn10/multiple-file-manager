f629a99acc2f8fcd8565d493c77a06da912662be
dhanyn10 <dhanyn.dhan@gmail.com>
2020-05-02 15:28:35 +0700

    add component listfile

---
+++ src/components/ListFile.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <b-list-group>
+            <b-list-group-item>list 1</b-list-group-item>
+            <b-list-group-item>list 2</b-list-group-item>
+            <b-list-group-item>list 3</b-list-group-item>
+        </b-list-group>
+    </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import { BListGroup, BListGroupItem } from 'bootstrap-vue'
+
+Vue.component('b-list-group', BListGroup)
+Vue.component('b-list-group-item', BListGroupItem)
+
+export default {
+    name: 'ListFile'
+}
+</script>
