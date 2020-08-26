da15fc9b2be36093c3f4ad2eeece59b1753aac58
dhanyn10 <dhanyn.dhan@gmail.com>
2020-08-27 00:43:13 +0700

    icon for navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,19 +1,34 @@
 <template>
     <b-navbar toggleable="sm" type="dark" variant="info" class="mb-4">
-        <b-navbar-nav class="ml-auto">
-            <b-nav-item href="#">link 1</b-nav-item>
-            <b-nav-item href="#">link 2</b-nav-item>
-        </b-navbar-nav>
+        <b-navbar-toggle target="navbar-toggle-collapse">
+            <template v-slot:default="{ expanded }">
+                <b-icon v-if="expanded" icon="list">
+                </b-icon>
+                <b-icon v-else icon="list">
+                </b-icon>
+            </template>
+        </b-navbar-toggle>
+        <b-collapse id="navbar-toggle-collapse" is-nav>
+            <b-navbar-nav class="ml-auto">
+                <b-nav-item href="#">link 1</b-nav-item>
+                <b-nav-item href="#">link 2</b-nav-item>
+            </b-navbar-nav>
+        </b-collapse>
     </b-navbar>
 </template>
 
 <script>
 import Vue from 'vue'
-import { BNavbar, BNavbarNav, BNavItem } from 'bootstrap-vue'
-
+import {
+    BNavbar,
+    BNavbarNav,
+    BNavItem,
+    BIcon
+    } from 'bootstrap-vue'
 Vue.component('b-navbar', BNavbar)
 Vue.component('b-navbar-nav', BNavbarNav)
 Vue.component('b-nav-item', BNavItem)
+Vue.component('b-icon', BIcon)
 
 export default {
     name: 'Navbar'
