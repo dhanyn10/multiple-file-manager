644537316b99990c793b54a2acc985e35b83f01a
dhanyn10 <dhanyn.dhan@gmail.com>
2020-08-27 07:46:29 +0700

    update config

--- src/background.js
+++ src/background.js
@@ -16,9 +16,13 @@ protocol.registerSchemesAsPrivileged([{scheme: 'app', privileges: { secure: true
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600, webPreferences: {
-    nodeIntegration: true
-  } })
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
