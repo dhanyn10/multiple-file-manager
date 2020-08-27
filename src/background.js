19574419ac44750c09a246f4945fcdbcfa04643b
dhanyn10 <dhanyn.dhan@gmail.com>
2020-08-28 02:30:37 +0700

    install custom titlebar

--- src/background.js
+++ src/background.js
@@ -34,6 +34,7 @@ function createWindow () {
     win = new BrowserWindow({
         width: 800,
         height: 600,
+        frame: false,
         webPreferences: {
             nodeIntegration: true
         }
