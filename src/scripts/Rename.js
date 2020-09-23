4ce6163488e10658b37d600ee8f0a30632fd659a
dhanyn10 <dhanyn.dhan@gmail.com>
2020-09-23 16:14:44 +0700

    rename function

--- src/scripts/Rename.js
+++ src/scripts/Rename.js
@@ -1,4 +1,4 @@
-// const filesystem = require('fs')
+const fs = require('fs')
 
 export const Rename = {
     
@@ -14,10 +14,24 @@ export const Rename = {
         var fulldir = val.fulldir,
             oldname = val.oldname,
             newname = val.newname
-        
-        console.log("dir", fulldir)
-        console.log("oldname", oldname)
-        console.log("newname", newname)
+
+        // converting backward slash to forward slash
+        fulldir = fulldir.replace(/\\/g, "/")
+        var dirlength = fulldir.length
+
+        //insert forward slash at the end of directory name
+        var dirlastindex = fulldir.substring(dirlength-1, dirlength)
+        if(dirlastindex != "/")
+        {
+            fulldir += "/"
+        }
+
+        //renaming file
+        fs.renameSync(fulldir + oldname, fulldir + newname, function(error){
+            if(error){
+                console.log(error)
+            }
+        })
     },
 
     deleteFunc: function(val)
