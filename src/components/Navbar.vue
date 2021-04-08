dc073fe4fbf3d9bce83ab70fd5085dcdda68f057
dhanyn10 <dhanyn.dhan@gmail.com>
2021-04-09 01:20:34 +0700

    rename to toggleDevtools

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,7 +5,7 @@
                 <b-nav-item v-on:click="visitWiki">Wiki</b-nav-item>
                 <b-nav-item-dropdown text="Help">
                     <b-dropdown-item v-on:click="visitIssues">Report Issue</b-dropdown-item>
-                    <b-dropdown-item v-on:click="openDevtools">Toggle Developer Tools</b-dropdown-item>
+                    <b-dropdown-item v-on:click="toggleDevtools">Toggle Developer Tools</b-dropdown-item>
                 </b-nav-item-dropdown>
             </b-navbar-nav>
         </b-navbar>
@@ -33,7 +33,7 @@ export default {
         visitIssues() {
             shell.openExternal('https://github.com/dhanyn10/multiple-file-manager/issues')
         },
-        openDevtools() {
+        toggleDevtools() {
             remote.getCurrentWindow().toggleDevTools()
         }
     }
