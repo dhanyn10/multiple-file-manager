1710fe310da25dbca195be1b7548cab8a24ad5e1
dhanyn10 <dhanyn.dhan@gmail.com>
2025-10-15 14:19:13 +0700

    update component navbar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,9 +1,9 @@
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, NavbarBrand } from 'react-bootstrap';
 
 function NavigationBar() {
   return (
     <Navbar bg="dark" variant="dark">
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <NavbarBrand href="#home">React-Bootstrap</NavbarBrand>
       <Nav className="me-auto">
         <Nav.Link href="#home">Home</Nav.Link>
         <Nav.Link href="#link">Link</Nav.Link>
